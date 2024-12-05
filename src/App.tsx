f98a3b517965cb281b56ce18fa8353cfc130d69b
nettotkm <netto.mtx@gmail.com>
2024-12-05 17:07:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>To Do list</h1>
+      <div className="card">
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
