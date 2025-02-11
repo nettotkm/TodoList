6f57b23390ba859062f001568bfe84b935965520
nettotkm <netto.mtx@gmail.com>
2025-02-11 18:00:38 -0300

    feat: add delete all todos and layout tweaks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "@material-tailwind/react";
 import { Checkbox } from "@material-tailwind/react";
 import { v4 as uuidv4 } from "uuid";
+import { TrashIcon } from "@heroicons/react/24/solid";
 interface TodoItem {
   id: string;
   description: string;
@@ -32,52 +33,79 @@ function TodoList() {
       })
     );
   };
+  const deleteTodo = (id: string) => {
+    setTodos(
+      todos.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
   console.log(todos);
   return (
-    <div className="bg-blue-gray-500 flex flex-col items-center ">
-      <h1 className="text-3xl font-bold underline">To Do list</h1>
+    <div className="bg-blue-gray-500 flex flex-col items-center w-full h-full  ">
+      <div className="bg-deep-purple-600 flex flex-col rounded-2xl m-10 w-180 ">
+        <h1 className="text-3xl font-bold underline pt-6 text-center">
+          To Do list
+        </h1>
 
-      <div className="w-full max-w-sm min-w-[200px] flex items-center gap-2">
-        <input
-          className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-hidden focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-          placeholder="New Todo..."
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-        />
-        <Button
-          className="rounded-full color=blue sm"
-          onClick={addTodo}
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          Add Todo
-        </Button>
-      </div>
-      <div className="">
-        <ul className="flex flex-col">
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              {" "}
-              <Checkbox
-                className="peer rounded-full"
-                color="blue"
-                crossOrigin={undefined}
-                onChange={() => handlechange(todo.id)}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              />
-              <span
-                className={`peer-checked:line-through text-red-200 ${
-                  todo.checked ? "line-through" : ""
-                }`}
+        <div className=" flex gap-4 p-6 py-4 ps-5 ">
+          <input
+            className="min-w-[520px]  bg-transparent border-amber-300 placeholder:text-gray-500  text-lg border rounded-md px-2 transition duration-300 ease focus:outline-hidden focus:border-b-gray-500 hover:border-slate-300 shadow-sm focus:shadow"
+            placeholder="New Todo..."
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button
+            className="rounded-xl bg-blue-500 sm px-6 py-2 border-2 border-gray-200"
+            onClick={addTodo}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Add Todo
+          </Button>
+        </div>
+        <div className="flex w-full   ">
+          <ul className="flex-1 pb-2 ">
+            {todos.map((todo) => (
+              <li
+                className="flex items-center  border-4 rounded-2xl m-2"
+                key={todo.id}
               >
-                {todo.description}
-              </span>
-            </li>
-          ))}
-        </ul>
+                {" "}
+                <Checkbox
+                  className="peer rounded-full"
+                  color="blue"
+                  crossOrigin={undefined}
+                  onChange={() => handlechange(todo.id)}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                />
+                <span
+                  className={`peer-checked:line-through text-2xl text-blue-gray-50  ${
+                    todo.checked ? "line-through" : ""
+                  }`}
+                >
+                  {todo.description}
+                </span>
+                <TrashIcon
+                  className="size-6 text-red-500 ml-auto mr-4"
+                  onClick={() => deleteTodo(todo.id)}
+                />
+                {/* <Button
+                  className="rounded-xl bg-blue-gray-700 sm px-6 py-2 border-4 border-amber-300"
+                  onClick={() => deleteTodo(todo.id)}
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Remove
+                </Button> */}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
